3fd158744ba8137fbb550c2e2b1a10cfa11cbbfc
Jan Temmerman <jantemme@student.arteveldehs.be>
2019-05-21 15:26:14 +0200

    site added

---
+++ docs/website/js/main.js
@@ -0,0 +1,47 @@
+setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.how').offset().top
+        }, 500);
+    }, 10000)
+
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.screens').offset().top
+        }, 500);
+    }, 20000)
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.team').offset().top
+        }, 500);
+    }, 30000)
+
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.about').offset().top
+        }, 500);
+    }, 40000)
+
+setInterval(function(){
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.how').offset().top
+        }, 500);
+    }, 10000)
+
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.screens').offset().top
+        }, 500);
+    }, 20000)
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.team').offset().top
+        }, 500);
+    }, 30000)
+
+    setTimeout(function () {
+        $('html, body').animate({
+            scrollTop: $('.about').offset().top
+        }, 500);
+    }, 40000)
+}, 40000)
